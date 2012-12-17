19f6cd82be597991fc80c92742a76564df82302f
Christopher Toledo <uberscientist@gmail.com>
2012-12-17 13:19:24 -0700

    Split vein and pulsar to two servers

--- pulsar_server.js
+++ pulsar_server.js
@@ -1,27 +1,26 @@
+/**
+* 4000: static server
+* 4001: pulsar server
+* 4002: vein server
+**/
 var http = require('http');
+var Pulsar = require('pulsar');
 
 var connect = require('connect');
+
 var app = connect();
-app.use(connect.static('static'));
+app.use(connect.static('static_pulsar'));
 app.use(function(req, res, next){
   console.log('connect req:', req.headers);
   next();
 });
+
 app.listen(4000);
 
 var pulsarServer = http.createServer(function(req, res){
   console.log('pulsar req:', req.headers);
 }).listen(4001);
 
-/**
-var veinServer = http.createServer(function(req, res){
-  console.log('vein req:', req.headers);
-}).listen(4002);
-
-var Vein = require('vein');
-**/
-
-var Pulsar = require('pulsar');
 var options = {
   host: 'localhost',
   port: 4001,
---
+++ vein_server.js
@@ -0,0 +1,30 @@
+/**
+* 4000: static server
+* 4001: pulsar server
+* 4002: vein server
+**/
+var http = require('http');
+
+var connect = require('connect');
+var app = connect();
+app.use(connect.static('static_vein'));
+app.use(function(req, res, next){
+  console.log('connect req:', req.headers);
+  next();
+});
+app.listen(4000);
+
+var Vein = require('vein');
+
+var veinServer = {
+  server:
+    http.createServer(function(req, res){
+      console.log('vein req:', req.headers);
+    }).listen(4002)
+}
+
+var vein = Vein.createServer(veinServer);
+
+vein.add('test', function(res, a, b){
+  res.reply('Recieved:' + a + ', ' + b);
+});

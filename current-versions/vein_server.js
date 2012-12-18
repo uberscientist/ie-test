65e20bb461557289e9d9d578bb7a05bdceb34ac9
Christopher Toledo <uberscientist@gmail.com>
2012-12-18 14:23:12 -0700

    Corrected vein initialization signature

--- current-versions/vein_server.js
+++ current-versions/vein_server.js
@@ -12,14 +12,14 @@ var app = connect();
 
 app.use(connect.static('static_vein'));
 app.use(function(req, res, next){
-  //console.log('connect req:\n', req.headers);
+  console.log('connect req:\n', req.headers);
   next();
 });
 
 app.listen(4000);
 
 var veinServer = http.createServer().listen(4002);
-var vein = Vein.createServer(veinServer);
+var vein = Vein.createServer({server: veinServer});
 
 vein.add('test', function(res, a, b){
   res.reply('Recieved:' + a + ', ' + b);
